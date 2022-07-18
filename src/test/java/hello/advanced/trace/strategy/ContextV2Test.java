4afccb07c0c5f3f39c12106f6973b2f0c37b9b1f
sungsanghyun <sos9210@naver.com>
2022-07-19 01:02:05 +0900

    전략패턴/템플릿콜백패턴

---
+++ src/test/java/hello/advanced/trace/strategy/ContextV2Test.java
@@ -0,0 +1,41 @@
+package hello.advanced.trace.strategy;
+
+import hello.advanced.trace.strategy.code.strategy.*;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ContextV2Test {
+
+    //전략패턴적용
+    @Test
+    void strategyV1() {
+        ContextV2 context = new ContextV2();
+        context.execute(new StrategyLogic1());
+        context.execute(new StrategyLogic2());
+    }
+    //전략패턴(익명클래스)적용
+    @Test
+    void strategyV2() {
+        ContextV2 context = new ContextV2();
+        context.execute(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스로직1 실행");
+            }
+        });
+        context.execute(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스로직2 실행");
+            }
+        });
+    }
+    //전략패턴(람다)적용
+    @Test
+    void strategyV3() {
+        ContextV2 context = new ContextV2();
+        context.execute(() -> log.info("비즈니스로직1 실행"));
+        context.execute(() -> log.info("비즈니스로직2 실행"));
+    }
+}
